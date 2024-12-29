e4473e9697a77d7f731ba52da31a11047073be72
HiroFr <ichiro.vf@gmail.com>
2024-12-29 20:57:58 +0100

    Refactor Slider component and unique keys for mapped elements

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,49 +8,50 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtB.date) - new Date(evtA.date)
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setIndex(index < byDateDesc.length - 1 ? index + 1 : 0)
   };
-  useEffect(() => {
-    nextCard();
-  });
+
+  useEffect(() => { // useEffect to change card every 5 seconds
+    const timeout = setTimeout(() => {
+      nextCard(); // call nextCard function
+    }, 5000);
+    return () => clearTimeout(timeout); // clear timeout on unmount
+  }, [nextCard]); // run useEffect when nextCard function changes
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
+        <div key={index}> {/* key should be unique */}
+        <div
+          key={event.id}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
           >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
+        </div>
+        <div className="SlideCard__paginationContainer">
+          <div className="SlideCard__pagination">
+            {byDateDesc.map((_, radioIdx) => (
+              <input
+                key={`${index}`} /* key should be unique */
+                type="radio"
+                name="radio-button"
+                checked={index === radioIdx}
+                onChange={() => setIndex(radioIdx)}
+              />
+            ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
