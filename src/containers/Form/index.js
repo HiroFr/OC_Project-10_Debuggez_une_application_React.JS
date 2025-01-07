7fc3104940981ea2a8aa90489e779709294c1aa2
HiroFr <ichiro.vf@gmail.com>
2025-01-07 19:52:18 +0100

    fix call onSuccess function

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // call the onSuccess function
       } catch (err) {
         setSending(false);
         onError(err);
